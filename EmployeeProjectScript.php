df22c3764547dbf9f4601ed8379caed59ea10814
yuuchin <shontzuforever@gmail.com>
2022-03-10 17:04:38 +0800

    deleted files

--- EmployeeProjectScript.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	$EmpEmail = (isset($_GET['EmpEmail']) ? $_GET['EmpEmail'] : '');
-	require_once ('connection.php');
-	$sql = "SELECT * FROM `project` WHERE EmpEmail = '$EmpEmail'";
-	$result = mysqli_query($con, $sql);
-	
-?>
-
-
--- connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","fypems") or die(mysqli_error($con));
-
-?>
